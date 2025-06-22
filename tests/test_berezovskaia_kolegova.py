f16f1ff71353941da6ae69e4226131015a1c7a05
losiik <49565548+losiik@users.noreply.github.com>
2025-06-22 15:02:26 +0300

    wait for element

--- tests/test_berezovskaia_kolegova.py
+++ tests/test_berezovskaia_kolegova.py
@@ -18,6 +18,7 @@ class TestKolegova(unittest.TestCase):
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--no-sandbox")
 
         service = ChromeService(executable_path=ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
